c6e6cc3d84f01d31d5660b2e51668327f8163f66
nizamudheenknr <nizamudheenknr@gmail.com>
2024-07-29 16:45:26 +0530

    user profile

--- Front-End/e-commerce-babysite/src/Page/UserProfile.jsx
+++ Front-End/e-commerce-babysite/src/Page/UserProfile.jsx
@@ -1,35 +1,35 @@
-// import React from 'react';
-// import './UserProfile.css'; // Import custom CSS
-// import Navbar from '../Component/Navbar';
+import React from 'react';
+import './UserProfile.css'; // Import custom CSS
+import Navbar from '../Component/Navbar';
 
 
-// const UserProfile = () => {
+const UserProfile = () => {
     
-//     const username = localStorage.getItem("user.name") 
-//     const email = localStorage.getItem("user.email") 
+    const username = localStorage.getItem("user.name") 
+    const email = localStorage.getItem("user.email") 
    
 
-//     return (
-//         <>
-//             <Navbar />
-//             <div className="profile-container">
-//                 <div className="profile-card">
-//                     <div className="profile-header">
-//                         {/* <img src=""alt="Profile" className="profile-pic" /> */}
-//                         <h2 className="card-title">User Profile:-</h2>
-//                     </div>
-//                     <div className="profile-info">
-//                         <div className="info-item">
-//                             <strong>User Name:</strong> {username}
-//                         </div>
-//                         <div className="info-item">
-//                             <strong>Email:</strong> {email}
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// }
+    return (
+        <>
+            <Navbar />
+            <div className="profile-container">
+                <div className="profile-card">
+                    <div className="profile-header">
+                        {/* <img src=""alt="Profile" className="profile-pic" /> */}
+                        <h2 className="card-title">User Profile:-</h2>
+                    </div>
+                    <div className="profile-info">
+                        <div className="info-item">
+                            <strong>User Name:</strong> {username}
+                        </div>
+                        <div className="info-item">
+                            <strong>Email:</strong> {email}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
 
-// export default UserProfile;
+export default UserProfile;
